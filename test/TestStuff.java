0601e1254228239d2f90792c9fa23ef4b45127dc
Aki Salmi <aki.salmi@iki.fi>
2016-09-17 11:15:36 +0300

    refactoring code

--- test/TestStuff.java
+++ test/TestStuff.java
@@ -50,20 +50,27 @@ public class TestStuff {
 
     @Test
     public void testUppercaseAMapsToUppercaseN() throws Exception {
-        String a = "A";
-
-        String result = CesarCipher.encode(a);
+        String result = CesarCipher.encode("A");
 
         assertThat(result, equalTo("N"));
+    }
+
+    @Test
+    public void testWithSpecialChars() throws Exception {
+
+        String result = CesarCipher.encode("A!");
+
+        assertThat(result, equalTo("N!"));
+
 
     }
 
-
     private String createARandomString(Character[] characters) {
         StringBuffer buffer = new StringBuffer();
-            Random random = new Random();
-        for( int i = 0; i < 100; i++){
-            int index = random.nextInt(5);
+        Random random = new Random();
+        int randomStringLength = random.nextInt(100) + 1;
+        for( int i = 0; i < randomStringLength; i++){
+            int index = random.nextInt(characters.length);
             buffer.append(characters[index] );
         }
         return  buffer.toString();
@@ -76,17 +83,32 @@ public class TestStuff {
         public static String encode(String string){
             StringBuilder inProgress = new StringBuilder();
             for(int i = 0; i < string.length(); i++) {
-                int encodedCharacter = string.charAt(i);
-                if( characters.contains( (char) encodedCharacter )){
-                    if(encodedCharacter >= 'N') {
-                        encodedCharacter -= 13;
-                    } else {
-                        encodedCharacter += 13;
-                    }
-                }
-                inProgress.append((char)encodedCharacter);
+                int plaintextCharacter = string.charAt(i);
+                Character toBeAppended;
+                toBeAppended = encodeCharacter((char) plaintextCharacter);
+                inProgress.append( toBeAppended);
             }
             return inProgress.toString().toUpperCase();
         }
+
+        private static Character encodeCharacter(char plaintextCharacter) {
+            if(isCharacterInTheAlphabetOfCharsToBeChanged(plaintextCharacter)){
+                return new Character(encrypt(plaintextCharacter));
+            } else {
+                return new Character(plaintextCharacter);
+            }
+        }
+
+        private static boolean isCharacterInTheAlphabetOfCharsToBeChanged(char plaintextCharacter) {
+            return characters.contains(plaintextCharacter);
+        }
+
+        private static char encrypt(char plaintextCharacter) {
+            int sizeOfTheAlphabet = characters.size();
+            int encryptionOffset = sizeOfTheAlphabet / 2;
+            int offsetOfPlaintextCharacter = characters.indexOf(plaintextCharacter);
+            return (char) characters.get((offsetOfPlaintextCharacter + encryptionOffset) % 6);
+        }
+
     }
 }
