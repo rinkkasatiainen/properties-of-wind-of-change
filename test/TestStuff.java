34f4c0824981772adbe908c8ce8171e3ea36f867
Aki Salmi <aki.salmi@iki.fi>
2016-09-17 10:19:18 +0300

    cleaning git

--- test/TestStuff.java
+++ test/TestStuff.java
@@ -1,5 +1,7 @@
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -17,13 +19,37 @@ public class TestStuff {
 
             assertThat(encode, equalTo( aRandomString ));
         }
+    }
 
+    @org.junit.Test
+    public void shouldBeTheSameAfterEncodingTwiceWithSpecialChars() throws Exception {
+        Character[] characters = {'A', 'B', 'C', 'N', 'O', 'P', ' ', '?' };
 
+        for( int i = 0; i < 100; i++){
+            String aRandomString = createARandomString(characters);
+            String encode = CesarCipher.encode(CesarCipher.encode(aRandomString));
+
+            assertThat(encode, equalTo( aRandomString ));
+        }
+    }
+
+    @org.junit.Test
+    public void shouldNotHaveLowercaseCharactersAfterEncoding() throws Exception {
+        Character[] characters = {'a', 'b', 'c', 'n', 'o', 'p'};
+
+        for( int i = 0; i < 100; i++){
+            String aRandomString = createARandomString(characters);
+            String encode = CesarCipher.encode(aRandomString);
+
+            for(int j = 0; j < encode.length(); j++) {
+                assertThat(encode.charAt(j) < (int) 'a', equalTo(true));
+            }
+        }
     }
 
 
     @Test
-    public void testName() throws Exception {
+    public void testUppercaseAMapsToUppercaseN() throws Exception {
         String a = "A";
 
         String result = CesarCipher.encode(a);
@@ -31,6 +57,7 @@ public class TestStuff {
         assertThat(result, equalTo("N"));
 
     }
+
 
     private String createARandomString(Character[] characters) {
         StringBuffer buffer = new StringBuffer();
@@ -43,18 +70,23 @@ public class TestStuff {
     }
 
     private static class CesarCipher {
+
+        private static List<Character> characters = Arrays.asList('A', 'B', 'C', 'N', 'O', 'P');
+
         public static String encode(String string){
             StringBuilder inProgress = new StringBuilder();
             for(int i = 0; i < string.length(); i++) {
                 int encodedCharacter = string.charAt(i);
-                if(encodedCharacter >= 'N') {
-                    encodedCharacter -= 13;
-                } else {
-                    encodedCharacter += 13;
+                if( characters.contains( (char) encodedCharacter )){
+                    if(encodedCharacter >= 'N') {
+                        encodedCharacter -= 13;
+                    } else {
+                        encodedCharacter += 13;
+                    }
                 }
                 inProgress.append((char)encodedCharacter);
             }
-            return inProgress.toString();
+            return inProgress.toString().toUpperCase();
         }
     }
 }
